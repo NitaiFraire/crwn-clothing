7118e18aa400458ab6bca99159d211436edb3fee
NitaiFraire <nfraire07@gmail.com>
2020-07-26 16:18:48 -0500

    using styled components instead regular styles in header, custom button, and homepage components

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,16 +1,31 @@
 import React from 'react';
 
-import './custom-button.styles.scss';
+/* ### regular scss ### */
+// import './custom-button.styles.scss';
 
-const CustomButton = ({ children, isGoogleSignIn, inverted, ...otherProps }) => (
-    <button 
-        className={`${inverted ? 'inverted' : ''}
-                    ${isGoogleSignIn ? 'google-sign-in' : ''}
-                    custom-button`}
-        {...otherProps}
-    >
+// const CustomButton = ({ 
+    //     children,
+    //     isGoogleSignIn, 
+    //     inverted, 
+    //     ...otherProps
+    // }) => (
+    //     <button 
+    //         className={`${inverted ? 'inverted' : ''}
+    //                     ${isGoogleSignIn ? 'google-sign-in' : ''}
+    //                     custom-button`}
+    //         {...otherProps}
+    //     >
+    //         {children}
+    //     </button>
+    // );
+        
+/* ### styled component ### */
+import { CustomButtonContainer } from './custom-button.styles'; 
+
+const CustomButton = ({ children, ...props }) => (
+    <CustomButtonContainer {...props}>
         {children}
-    </button>
+    </CustomButtonContainer>
 );
 
 export default CustomButton;
