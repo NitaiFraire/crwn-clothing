b7ae9a445a3e423c99aa8567a28e62146a126c1b
NitaiFraire <nfraire07@gmail.com>
2020-08-01 16:53:36 -0500

    collection component and collections-overview component transfered into container pattern

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -13,7 +13,7 @@ export const selectCollectionsForPreview = createSelector(
     [selectCollections],
     collections => 
         collections ? Object.keys(collections).map(key => collections[key]) : []
-)
+);
 
 export const selectCollection = memoize((collectionUrlParam) => 
     createSelector(
@@ -21,3 +21,13 @@ export const selectCollection = memoize((collectionUrlParam) =>
         collections => (collections ? collections[collectionUrlParam] : null)
     )
 );
+
+export const selectIsCollectionFetching = createSelector(
+    [selectShop],
+    shop => shop.isFetching
+);
+
+export const selectIsCollectionsLoaded = createSelector(
+    [selectShop],
+    shop => !!shop.collections
+)
