27990ceeac73d8eb1568178963b1034c614309dc
NitaiFraire <nfraire07@gmail.com>
2020-08-03 18:45:36 -0500

    updated signUp and signOut with sagas

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -1,4 +1,4 @@
-import { takeLatest, call, put } from 'redux-saga/effects';
+import { takeLatest, call, put, all } from 'redux-saga/effects';
 
 import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
 
@@ -27,4 +27,10 @@ export function* fetchCollectionStart() {
         ShopActionTypes.FETCH_COLLECTIONS_START,
         fetchCollectionAsync
     );
+};
+
+export function* shopSagas() {
+    yield all([
+        call(fetchCollectionStart),
+    ])
 }
